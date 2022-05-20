ef3609147b611d88c46bdbb7860b20d5a896da2d
Rodrigo <rodrigdsoto@gmail.com>
2022-05-20 17:15:31 -0300

    Signed-off-by: Rodrigo <rodrigdsoto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,21 @@ export class AppComponent implements OnInit {
   control_sidebar(){
     const btnMenu=document.getElementById("btnMenu");
     const sidebar=document.querySelector(".sidebar");
+    const menu=document.querySelector(".menu");
+    const item_menu=document.querySelectorAll(".item-menu a");
+    
     btnMenu?.addEventListener("click",()=>{
       sidebar?.classList.toggle("active");
     });
+    
+    for(let i=0;i<item_menu.length;i++){
+      item_menu[i].addEventListener("click",()=>{
+        for(let i=0;i<item_menu.length;i++){
+          item_menu[i].classList.remove("seleccionado");
+        }        
+        item_menu[i].classList.add("seleccionado");
+      }); 
+    }
     
   }
 }
