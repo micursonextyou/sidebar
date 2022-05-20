211d12e70dc6ed20e31e78689184545619677377
Rodrigo <rodrigdsoto@gmail.com>
2022-05-20 13:15:31 -0300

    Signed-off-by: Rodrigo <rodrigdsoto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.control_sidebar();
+  }
   title = 'sidebar3';
+  
+  control_sidebar(){
+    const btnMenu=document.getElementById("btnMenu");
+    const sidebar=document.querySelector(".sidebar");
+    btnMenu?.addEventListener("click",()=>{
+      sidebar?.classList.toggle("active");
+    });
+    
+  }
 }
